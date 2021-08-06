63e4a3959f1b440ef622a1a4891d8d326dc9a490
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-08-06 23:43:31 +0700

    add link

--- application/views/template.php
+++ application/views/template.php
@@ -109,7 +109,7 @@
 				</a>
 			</li>
 			<li class="nav-item">
-				<a href="../widgets.html" class="nav-link">
+				<a href="<?=site_url('supplier')?>" class="nav-link">
 				<i class="nav-icon fas fa-truck"></i>
 				<p>
 					Suppliers
